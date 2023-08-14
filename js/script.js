7d72527d074b986a5a3a6f200134f849139e127c
michelecanini <studio.michelecanini@gmail.com>
2023-08-14 20:47:14 +0200

    [NEW] added project 02

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// ARRAY DEI PROGETTI
 let dataProjects = [
     {
         title: "Scooter Lim",
@@ -8,17 +9,36 @@ let dataProjects = [
         demo: "https://michelecanini.github.io/scooterlimproject/"
     },
     {
+        title: "Airbnb Blocks",
+        thumb: "https://github.com/michelecanini/airbnb-blocks/raw/main/img/airbnb-blocks.png",
+        github: "https://github.com/michelecanini/airbnb-blocks",
+        demo: "https://michelecanini.github.io/airbnb-blocks/"
+    },
+    {
         title: "Wayne Barnett",
         thumb: "https://github.com/michelecanini/scooterlimproject/raw/main/img/CodingChallenge.png",
         github: "Founder & CEO",
         demo: "wayne-barnett-founder-ceo.jpg"
     },
     {
-        title: "Wayne Barnett",
-        thumb: "Wayne Barnett",
+        title: "Test",
+        thumb: "https://github.com/michelecanini/scooterlimproject/raw/main/img/CodingChallenge.png",
         github: "Founder & CEO",
         demo: "wayne-barnett-founder-ceo.jpg"
-    }
+    },
+    {
+        title: "Test",
+        thumb: "https://github.com/michelecanini/scooterlimproject/raw/main/img/CodingChallenge.png",
+        github: "Founder & CEO",
+        demo: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        title: "Test",
+        thumb: "https://github.com/michelecanini/scooterlimproject/raw/main/img/CodingChallenge.png",
+        github: "Founder & CEO",
+        demo: "wayne-barnett-founder-ceo.jpg"
+    },
+   
 ]
 
 // CONTENITORE DELLE SCHEDE NEL DOM
@@ -34,18 +54,14 @@ for (let i = 0; i < dataProjects.length; i++){
     let card = 
     
     `
-    <section class="container">
-        <div class="row">
-            <div class="card col-12 col-md-4 m-3 text-white border border-primary" style="width: 25rem; background-color: #2d3748;" >
-                <img src="${project.thumb}" class="card-img-top mt-3">
-                <div class="card-body">
-                    <h5 class="card-title">${project.title}</h5>
-                    <a href="${project.github}" target="_blank" class="btn btn-primary">GitHub</a>
-                    <a href="${project.demo}" target="_blank" class="btn btn-light">Demo</a>
-                </div>
-            </div>
+    <div class="card col-12 col-md-4 m-3 text-white border border-primary" style="width: 25rem; background-color: #2d3748; max-height: 320px;" >
+        <div class="card-body">
+        <img src="${project.thumb}" class="card-img-top mt-3">
+            <h5 class="card-title">${project.title}</h5>
+            <a href="${project.github}" target="_blank" class="btn btn-primary">GitHub</a>
+            <a href="${project.demo}" target="_blank" class="btn btn-light">Demo</a>
         </div>
-    </section>
+    </div>
     `
 
     //CONCATENAZIONE HTML DELLA NUOVA CARD
@@ -55,3 +71,4 @@ for (let i = 0; i < dataProjects.length; i++){
 //STYLE CSS CARDS
 document.getElementById("cards").style.display = "flex";
 document.getElementById("cards").style.flexDirection = "row";
+document.getElementById("cards").style.flexWrap = "wrap";
