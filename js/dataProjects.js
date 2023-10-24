ec75ce5713daf955e298149404888851026a91fe
michelecanini <studio.michelecanini@gmail.com>
2023-10-24 11:09:37 +0200

    [TWEAK] dataProjects

--- js/dataProjects.js
+++ js/dataProjects.js
@@ -1,34 +1,11 @@
 // ARRAY DEI PROGETTI
 let dataProjects = [
-    {
-        title: "Scooter Lim",
-        thumb: "https://github.com/michelecanini/scooterlimproject/raw/main/img/CodingChallenge-min.png",
-        github: "https://github.com/michelecanini/scooterlimproject",
-        demo: "https://michelecanini.github.io/scooterlimproject/"
-    },
+
     {
         title: "Airbnb Blocks",
         thumb: "https://github.com/michelecanini/airbnb-blocks/raw/main/img/airbnb-blocks.png",
         github: "https://github.com/michelecanini/airbnb-blocks",
         demo: "https://michelecanini.github.io/airbnb-blocks/"
-    },
-    {
-        title: "Catalogo Ecommerce",
-        thumb: "https://github.com/michelecanini/catalogoecommerce/raw/main/img/e-shop.png",
-        github: "https://github.com/michelecanini/catalogoecommerce",
-        demo: "https://michelecanini.github.io/catalogoecommerce/"
-    },
-    {
-        title: "Album Fotografico",
-        thumb: "https://github.com/michelecanini/albumfotografico/raw/main/albums.png",
-        github: "https://github.com/michelecanini/albumfotografico",
-        demo: "https://michelecanini.github.io/albumfotografico/"
-    },
-    {
-        title: "Youtube Channel",
-        thumb: "https://github.com/michelecanini/youtube-channel/raw/main/img/youtube_channel.png",
-        github: "https://github.com/michelecanini/youtube-channel",
-        demo: "https://michelecanini.github.io/youtube-channel/"
     },
     {
         title: "Toboolist",
@@ -41,12 +18,6 @@ let dataProjects = [
         thumb: "https://github.com/michelecanini/htmlcss-shoes/raw/master/img/shoes.png",
         github: "https://github.com/michelecanini/htmlcss-shoes",
         demo: "https://michelecanini.github.io/htmlcss-shoes/"
-    },
-    {
-        title: "Float Cards",
-        thumb: "https://github.com/michelecanini/html-css-float-cards/raw/master/img/float_cards.png",
-        github: "https://github.com/michelecanini/html-css-float-cards",
-        demo: "https://michelecanini.github.io/html-css-float-cards/"
     },
     {
         title: "Boolando Shop",
@@ -90,24 +61,6 @@ let dataProjects = [
         github: "https://github.com/michelecanini/html-css-bootstrap-dashboard",
         demo: "https://michelecanini.github.io/html-css-bootstrap-dashboard/"
     },
-    //{
-    //    title: "Responsive Wannabe",
-    //    thumb: "https://github.com/michelecanini/html-css-resp-wannabe/raw/master/img/responsive_wannabe.png",
-    //    github: "https://github.com/michelecanini/html-css-resp-wannabe",
-    //    demo: "https://michelecanini.github.io/html-css-resp-wannabe/"
-    //},
-    //{
-    //    title: "Responsive Wannabe",
-    //    thumb: "https://github.com/michelecanini/html-css-resp-wannabe/raw/master/img/responsive_wannabe.png",
-    //    github: "https://github.com/michelecanini/html-css-resp-wannabe",
-    //    demo: "https://michelecanini.github.io/html-css-resp-wannabe/"
-    //},
-    //{
-    //    title: "Responsive Wannabe",
-    //    thumb: "https://github.com/michelecanini/html-css-resp-wannabe/raw/master/img/responsive_wannabe.png",
-    //    github: "https://github.com/michelecanini/html-css-resp-wannabe",
-    //    demo: "https://michelecanini.github.io/html-css-resp-wannabe/"
-    //},
    
 ]
 
