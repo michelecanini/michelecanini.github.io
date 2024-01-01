63594ce867b9ce1620d311e475d648cdee65553f
michelecanini <studio.michelecanini@gmail.com>
2024-01-01 19:06:11 +0100

    [FEAT] Added footer and favicon

--- js/dataProjects.js
+++ js/dataProjects.js
@@ -1,55 +1,53 @@
-// ARRAY DEI PROGETTI
+// project arrays
 let dataProjects = [
-
-    {
-        title: "Spotify",
-        thumb: "https://github.com/michelecanini/html-css-spotifyweb/raw/master/img/spotify-min.png",
-        github: "https://github.com/michelecanini/html-css-spotifyweb",
-        demo: "https://michelecanini.github.io/html-css-spotifyweb/"
-    },
-    {
-        title: "Bootstrap Freelance",
-        thumb: "https://github.com/michelecanini/html-css-bootstrap-freelancer/raw/master/img/bootstrap_freelance.png",
-        github: "https://github.com/michelecanini/html-css-bootstrap-freelancer",
-        demo: "https://michelecanini.github.io/html-css-bootstrap-freelancer/"
-    },
-    {
-        title: "Bootstrap Dasboard",
-        thumb: "https://github.com/michelecanini/html-css-bootstrap-dashboard/raw/master/img/bootstrap_dasboard.png",
-        github: "https://github.com/michelecanini/html-css-bootstrap-dashboard",
-        demo: "https://michelecanini.github.io/html-css-bootstrap-dashboard/"
-    },
-    {
-        title: "Campo Minato",
-        thumb: "https://github.com/michelecanini/js-campominato-dom/raw/master/img/campominato-min.png",
-        github: "https://github.com/michelecanini/js-campominato-dom",
-        demo: "https://michelecanini.github.io/js-campominato-dom/"
-    },
-    {
-        title: "Vue Todolist",
-        thumb: "https://github.com/michelecanini/vue-todolist/raw/main/img/vuetodolist.png",
-        github: "https://github.com/michelecanini/vue-todolist",
-        demo: "https://michelecanini.github.io/vue-todolist/"
-    },
-    {
-        title: "Boolzapp Web",
-        thumb: "https://github.com/michelecanini/vue-boolzapp/raw/main/img/boolzapp.png",
-        github: "https://github.com/michelecanini/vue-boolzapp",
-        demo: "https://michelecanini.github.io/vue-boolzapp/"
-    },
-    {
-        title: "Vite Boolflix",
-        thumb: "https://github.com/michelecanini/vite-boolflix/raw/main/src/assets/images/boolflix-min.png",
-        github: "https://github.com/michelecanini/vite-boolflix",
-        demo: "https://michelecanini-boolflix.netlify.app/"
-    },
-    {
-        title: "Laravel Cosmetics",
-        thumb: "https://github.com/michelecanini/laravel-retail-cosmetics/raw/main/public/img/laravelcosmetics.png",
-        github: "https://github.com/michelecanini/laravel-retail-cosmetics",
-        demo: "https://retailcosmetics.eu"
-    },
-   
+  {
+    title: "Spotify",
+    thumb: "https://github.com/michelecanini/html-css-spotifyweb/raw/master/img/spotify-min.png",
+    github: "https://github.com/michelecanini/html-css-spotifyweb",
+    demo: "https://michelecanini.github.io/html-css-spotifyweb/"
+  },
+  {
+    title: "Bootstrap Freelance",
+    thumb: "https://github.com/michelecanini/html-css-bootstrap-freelancer/raw/master/img/bootstrap_freelance.png",
+    github: "https://github.com/michelecanini/html-css-bootstrap-freelancer",
+    demo: "https://michelecanini.github.io/html-css-bootstrap-freelancer/"
+  },
+  {
+    title: "Bootstrap Dasboard",
+    thumb: "https://github.com/michelecanini/html-css-bootstrap-dashboard/raw/master/img/bootstrap_dasboard.png",
+    github: "https://github.com/michelecanini/html-css-bootstrap-dashboard",
+    demo: "https://michelecanini.github.io/html-css-bootstrap-dashboard/"
+  },
+  {
+    title: "Campo Minato",
+    thumb: "https://github.com/michelecanini/js-campominato-dom/raw/master/img/campominato-min.png",
+    github: "https://github.com/michelecanini/js-campominato-dom",
+    demo: "https://michelecanini.github.io/js-campominato-dom/"
+  },
+  {
+    title: "Vue Todolist",
+    thumb: "https://github.com/michelecanini/vue-todolist/raw/main/img/vuetodolist.png",
+    github: "https://github.com/michelecanini/vue-todolist",
+    demo: "https://michelecanini.github.io/vue-todolist/"
+  },
+  {
+    title: "Boolzapp Web",
+    thumb: "https://github.com/michelecanini/vue-boolzapp/raw/main/img/boolzapp.png",
+    github: "https://github.com/michelecanini/vue-boolzapp",
+    demo: "https://michelecanini.github.io/vue-boolzapp/"
+  },
+  {
+    title: "Vite Boolflix",
+    thumb: "https://github.com/michelecanini/vite-boolflix/raw/main/src/assets/images/boolflix-min.png",
+    github: "https://github.com/michelecanini/vite-boolflix",
+    demo: "https://michelecanini-boolflix.netlify.app/"
+  },
+  {
+    title: "Laravel Cosmetics",
+    thumb: "https://github.com/michelecanini/laravel-retail-cosmetics/raw/main/public/img/laravelcosmetics.png",
+    github: "https://github.com/michelecanini/laravel-retail-cosmetics",
+    demo: "https://retailcosmetics.eu"
+  },
 ]
 
 export { dataProjects };
--- js/script.js
+++ js/script.js
@@ -1,67 +1,49 @@
-"use strict";
-
-// IMPORTAZIONE DELL'ARRAY
+// import of the array
 import { dataProjects } from './dataProjects.js';
 
-// FUNZIONE DI CREAZIONE CARD
+// card creation function
 function createCard(project) {
-    let card = `
-        <div class="card card_style scale_card col-12 col-md-4 m-3 text-white border border-primary" style="max-width: 24rem; background-color: #2d3748;" >
-            <div class="card-body">
-                <img src="${project.thumb}" class="card-img-top mt-3">
-                <h5 class="card-title mt-3">${project.title}</h5>
-                <a href="${project.github}" target="_blank" class="btn btn-primary m-1">GitHub</a>
-                <a href="${project.demo}" target="_blank" class="btn btn-light m-1">Demo</a>
-            </div>
-        </div>
-    `;
-    return card;
+  let card = `
+    <div class="card card_style scale_card col-12 col-md-4 m-3">
+      <div class="card-body test">
+        <h5 class="card-title text-white mt-3">${project.title}</h5>
+        <img src="${project.thumb}" class="card-img-top mt-1">
+        <a href="${project.github}" target="_blank" class="btn btn-primary m-1 mt-3">GitHub</a>
+        <a href="${project.demo}" target="_blank" class="btn btn-light m-1 mt-3">Demo</a>
+      </div>
+    </div>
+  `;
+  return card;
 }
 
-// FUNZIONE DEL BOTTONE
+// button function
 function createButton(text, start, end) {
-    let button = document.createElement("button");
-    button.innerHTML = text;
-    button.classList.add("btn", "btn-success");
-    button.addEventListener("click", function() {
-        for (let i = end; i >= start; i--) {
-            let project = dataProjects[i];
-            let card = createCard(project);
-            cards.innerHTML += card;
-        }
-        button.style.display = "none";
-        buttonContainer.style.display = "none";
-    });
-    return button;
+  let button = document.createElement("button");
+  button.innerHTML = text;
+  button.classList.add("btn", "btn-success");
+  button.addEventListener("click", function() {
+    for (let i = end; i >= start; i--) {
+      let project = dataProjects[i];
+      let card = createCard(project);
+      cards.innerHTML += card;
+    }
+    button.style.display = "none";
+    buttonContainer.style.display = "none";
+  });
+  return button;
 }
 
-
-// CONTENITORE DELLE SCHEDE NEL DOM
+// container of the cards
 const cards = document.getElementById("cards");
 
-// CICLO FOR INVERTITO PER I PRIMI 3 PROGETTI
+// reversed FOR loop for the first 3 projects
 for (let i = dataProjects.length - 1; i >= dataProjects.length - 3; i--){
     let project = dataProjects[i];
     let card = createCard(project);
     cards.innerHTML += card;
 }
 
+// button to load projects
 const buttonContainer = document.getElementById("button-container");
-
-// PULSANTE PER CARICARE I PROSSIMI PROGETTI
 let loadMoreButton = createButton("Upload Projects", 0, 4);
 buttonContainer.appendChild(loadMoreButton);
-
-// PULSANTE PER CARICARE ULTERIORI 3 PROGETTI
-//let loadMoreButton2 = createButton("Upload Projects", 0, 2); 
-//loadMoreButton2.style.display = "none"; 
-//buttonContainer.appendChild(loadMoreButton2);
-
-loadMoreButton.addEventListener("click", function() {
-    loadMoreButton2.style.display = "inline-block"; 
-});
-
-//STYLE CSS CARDS
-document.getElementById("cards").style.display = "flex";
-document.getElementById("cards").style.flexDirection = "row";
-document.getElementById("cards").style.flexWrap = "wrap";
