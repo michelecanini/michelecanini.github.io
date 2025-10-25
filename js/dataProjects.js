e4a390219d03374c706403c5491c7094e3de1a5e
michelecanini <studio.michelecanini@gmail.com>
2025-10-25 18:52:44 +0200

    update

--- js/dataProjects.js
+++ js/dataProjects.js
@@ -2,53 +2,61 @@
 let dataProjects = [
   {
     title: "Spotify",
-    thumb: "https://github.com/michelecanini/html-css-spotifyweb/raw/master/img/spotify-min.png",
+    thumb:
+      "https://github.com/michelecanini/html-css-spotifyweb/raw/master/img/spotify-min.png",
     github: "https://github.com/michelecanini/html-css-spotifyweb",
-    demo: "https://michelecanini.github.io/html-css-spotifyweb/"
+    demo: "https://michelecanini.github.io/html-css-spotifyweb/",
   },
   {
     title: "Bootstrap Freelance",
-    thumb: "https://github.com/michelecanini/html-css-bootstrap-freelancer/raw/master/img/bootstrap_freelance.png",
+    thumb:
+      "https://github.com/michelecanini/html-css-bootstrap-freelancer/raw/master/img/bootstrap_freelance.png",
     github: "https://github.com/michelecanini/html-css-bootstrap-freelancer",
-    demo: "https://michelecanini.github.io/html-css-bootstrap-freelancer/"
+    demo: "https://michelecanini.github.io/html-css-bootstrap-freelancer/",
   },
   {
     title: "Bootstrap Dasboard",
-    thumb: "https://github.com/michelecanini/html-css-bootstrap-dashboard/raw/master/img/bootstrap_dasboard.png",
+    thumb:
+      "https://github.com/michelecanini/html-css-bootstrap-dashboard/raw/master/img/bootstrap_dasboard.png",
     github: "https://github.com/michelecanini/html-css-bootstrap-dashboard",
-    demo: "https://michelecanini.github.io/html-css-bootstrap-dashboard/"
+    demo: "https://michelecanini.github.io/html-css-bootstrap-dashboard/",
   },
   {
     title: "Campo Minato",
-    thumb: "https://github.com/michelecanini/js-campominato-dom/raw/master/img/campominato-min.png",
+    thumb:
+      "https://github.com/michelecanini/js-campominato-dom/raw/master/img/campominato-min.png",
     github: "https://github.com/michelecanini/js-campominato-dom",
-    demo: "https://michelecanini.github.io/js-campominato-dom/"
+    demo: "https://michelecanini.github.io/js-campominato-dom/",
   },
   {
     title: "Vue Todolist",
-    thumb: "https://github.com/michelecanini/vue-todolist/raw/main/img/vuetodolist.png",
+    thumb:
+      "https://github.com/michelecanini/vue-todolist/raw/main/img/vuetodolist.png",
     github: "https://github.com/michelecanini/vue-todolist",
-    demo: "https://michelecanini.github.io/vue-todolist/"
+    demo: "https://michelecanini.github.io/vue-todolist/",
   },
   {
     title: "Boolzapp Web",
-    thumb: "https://github.com/michelecanini/vue-boolzapp/raw/main/img/boolzapp.png",
+    thumb:
+      "https://github.com/michelecanini/vue-boolzapp/raw/main/img/boolzapp.png",
     github: "https://github.com/michelecanini/vue-boolzapp",
-    demo: "https://michelecanini.github.io/vue-boolzapp/"
+    demo: "https://michelecanini.github.io/vue-boolzapp/",
   },
   {
     title: "Vite Boolflix",
-    thumb: "https://github.com/michelecanini/vite-boolflix/raw/main/src/assets/images/boolflix-min.png",
+    thumb:
+      "https://github.com/michelecanini/vite-boolflix/raw/main/src/assets/images/boolflix.png",
     github: "https://github.com/michelecanini/vite-boolflix",
-    demo: "https://michelecanini-boolflix.netlify.app/"
+    demo: "https://michelecanini-boolflix.netlify.app/",
   },
   {
     title: "Laravel Cosmetics",
-    thumb: "https://github.com/michelecanini/laravel-retail-cosmetics/raw/main/public/img/laravelcosmetics.png",
+    thumb:
+      "https://github.com/michelecanini/laravel-retail-cosmetics/raw/main/public/img/laravelcosmetics.png",
     github: "https://github.com/michelecanini/laravel-retail-cosmetics",
-    demo: "https://retailcosmetics.eu"
+    demo: "https://retailcosmetics.eu",
   },
-]
+];
 
 // export of the array
 export { dataProjects };
